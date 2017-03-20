6a8355b8944a991ea35e87d9430d692caf695f66
chroda <chroda@chroda.com.br>
2017-03-20 16:28:56 -0300

    making the videos - v5yt

---
+++ hello-typescript/main.ts
@@ -0,0 +1,20 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x,y){
+  return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1,2,3];
+numeros.map(function(valor){
+  return valor * 2;
+})
+numeros.map(valor=>valor * 2);
+
+class Matematica{
+  soma(x,y){
+    return x+y;
+  }
+}
