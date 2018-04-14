4665a3e8dd3c2a1aea09d7ba78e62209bc251ab7
chroda <chroda@chroda.com.br>
2018-04-14 14:57:05 -0300

    example-vid-10

--- 2018_remenbering/data-binding/src/app/data-binding/data-binding.component.ts
+++ 2018_remenbering/data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url = 'http://loiane.com';
+  cursoAngular = true;
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
   constructor() { }
 
   ngOnInit() {
