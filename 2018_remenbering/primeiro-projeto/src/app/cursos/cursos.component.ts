144c43cf660a7a8a81110a4fcb846c7f4657814b
chroda <chroda@chroda.com.br>
2018-04-14 12:44:19 -0300

    example-vid-6

--- 2018_remenbering/primeiro-projeto/src/app/cursos/cursos.component.ts
+++ 2018_remenbering/primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training';
+  }
 
   ngOnInit() {
   }
