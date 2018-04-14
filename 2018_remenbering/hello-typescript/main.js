3355c335b1c59a4a4c98c9d3f7ee3ecc554991ed
chroda <chroda@chroda.com.br>
2018-04-14 11:27:39 -0300

    example 2

---
+++ 2018_remenbering/hello-typescript/main.js
@@ -0,0 +1,12 @@
+var minhaVar = 'minha variável';
+function minhaFunc2(x, y) {
+    return x + y;
+}
+// ES6 ou  ES2015
+var number = 2;
+var PII = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; }); // ES 2015
---
+++ 2018_remenbering/hello-typescript/main.ts
@@ -0,0 +1,16 @@
+var minhaVar = 'minha variável';
+
+function minhaFunc2(x,y){
+    return x + y;
+}
+
+// ES6 ou  ES2015
+let number = 2;
+const PII = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+
+numeros.map(valor => valor * 2); // ES 2015
