0b39f2119a4cca61bfe30b42079950b677d37dcb
Christian Oliveira <coliveira@quanticosolutions.com>
2022-11-28 16:10:58 +0000

    update course

---
+++ 2022-linkedin-learning/angular-essential-training/src/app/media-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.css']
+})
+export class MediaItemComponent {}
+
