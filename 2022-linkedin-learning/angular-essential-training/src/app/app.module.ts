93bf826bfe95b6046f62e9fd914de77c09daac0e
Christian Oliveira <coliveira@quanticosolutions.com>
2022-11-29 10:26:31 +0000

    update course

--- 2022-linkedin-learning/angular-essential-training/src/app/app.module.ts
+++ 2022-linkedin-learning/angular-essential-training/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
+import { MediaItemListComponent } from './media-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MediaItemComponent
+    MediaItemComponent,
+    MediaItemListComponent
   ],
   imports: [
     BrowserModule
--- 2022-linkedin-learning/angular-essential-training/src/app/media-item.component.ts
+++ 2022-linkedin-learning/angular-essential-training/src/app/media-item.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
   templateUrl: './media-item.component.html',
   styleUrls: ['./media-item.component.css']
 })
-export class MediaItemComponent {}
+export class MediaItemComponent {
+  @Input() mediaItem;
+  @Output() delete = new EventEmitter();
+
+  onDelete() {
+    console.log('deleted');
+    this.delete.emit(this.mediaItem);
+  }
+}
 
