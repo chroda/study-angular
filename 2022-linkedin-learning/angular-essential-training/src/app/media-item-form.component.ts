065209637d6dbfc124973d42f77f6d4bd22dae2a
Christian Oliveira <coliveira@quanticosolutions.com>
2022-11-30 11:16:45 +0000

    update course

--- 2022-linkedin-learning/angular-essential-training/src/app/media-item-form.component.ts
+++ 2022-linkedin-learning/angular-essential-training/src/app/media-item-form.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder  } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {}
+export class MediaItemFormComponent implements OnInit {
+  form: FormGroup; 
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+  
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      medium: this.formBuilder.control('Movies'),
+      name: this.formBuilder.control('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: this.formBuilder.control(''),
+      year: this.formBuilder.control('', this.yearValidator),  
+    })
+  }
+
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1800;
+    const maxYear = 2500;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return {
+        year: {
+          min: minYear,
+          max: maxYear
+        } 
+      }
+    }
+  }
+
+  onSubmit(mediaItem) {
+    console.log(mediaItem);
+    
+  }
+}
