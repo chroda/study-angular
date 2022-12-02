89ac5a7849e7086aee1a0766e9f0fef86087e029
Christian Oliveira <coliveira@quanticosolutions.com>
2022-12-02 16:38:22 +0000

    update course

--- 2022-linkedin-learning/angular-essential-training/src/app/category-list.component.ts
+++ 2022-linkedin-learning/angular-essential-training/src/app/category-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "mw-category-list",
+  selector: 'mw-category-list',
   template: `
     <span class="label" *ngFor="let category of categories">
       {{ category }}
@@ -10,35 +10,19 @@ import { Component, Input } from "@angular/core";
   styles: [
     `
       :host {
-        display: inline-block;
+        display: block;
         margin-bottom: 20px;
       }
-
       :host-context(.medium-movies) span {
         background-color: #53ace4;
       }
-
       :host-context(.medium-series) span {
         background-color: #45bf94;
       }
-
       span {
         display: inline-block;
         margin-right: 4px;
         margin-bottom: 4px;
-      }
-      
-      span:last-of-type {
-        margin-right: 0;
-      }
-
-      .label {
-        background-color: #999999;
-        color: #ffffff;
-        border-radius: 4px;
-        padding: 2px 6px;
-        font-style: italic;
-        width: max-content;
       }
     `
   ]
