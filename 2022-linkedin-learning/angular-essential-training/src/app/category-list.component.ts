45463683cbaffe489f981f793f8e71dc24daa82b
Christian Oliveira <coliveira@quanticosolutions.com>
2022-12-02 15:48:10 +0000

    update course

--- 2022-linkedin-learning/angular-essential-training/src/app/category-list.component.ts
+++ 2022-linkedin-learning/angular-essential-training/src/app/category-list.component.ts
@@ -9,11 +9,29 @@ import { Component, Input } from "@angular/core";
   `,
   styles: [
     `
+      :host {
+        display: inline-block;
+        margin-bottom: 20px;
+      }
+
+      :host-context(.medium-movies) span {
+        background-color: #53ace4;
+      }
+
+      :host-context(.medium-series) span {
+        background-color: #45bf94;
+      }
+
       span {
         display: inline-block;
         margin-right: 4px;
         margin-bottom: 4px;
       }
+      
+      span:last-of-type {
+        margin-right: 0;
+      }
+
       .label {
         background-color: #999999;
         color: #ffffff;
